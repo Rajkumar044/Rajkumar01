d44cc38d4ceea4e7dba4aa5501cd79d469bb4d8d
Rajkumar044 <rajdeva990@gmail.com>
2021-05-30 10:48:52 +0530

    git push

---
+++ src/org/git/GitPull.java
@@ -0,0 +1,11 @@
+package org.git;
+
+public class GitPull {
+public void name() {
+	System.out.println("raj");
+}
+public static void main(String[] args) {
+	GitPull a=new GitPull();
+	a.name();
+}
+}
